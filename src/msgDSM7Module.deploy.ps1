198cd2e56624ad3e690b8964b85ee903943b5744
Uwe Franke <uwe.franke@outlook.com>
2018-07-19 13:44:20 +0200

    1.0.1.5

--- src/msgDSM7Module.deploy.ps1
+++ src/msgDSM7Module.deploy.ps1
@@ -1,57 +1,107 @@
-<#
-.SYNOPSIS
-   Installiert Powershell Module msgDSM7Module
-.DESCRIPTION
-   Installiert Powershell Module msgDSM7Module
-.EXAMPLE
-   msgDSM7Module.deploy.ps1 
-#>
+
+<#PSScriptInfo
+
+.VERSION 1.1
+
+.GUID 936c87ee-e4be-494a-9388-b7d8b695e484
+
+.AUTHOR Uwe Franke
+
+.COMPANYNAME msg services AG
+
+.COPYRIGHT msg services AG
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+.PRIVATEDATA 
+
+#> 
+
+
+
+<# 
+
+.DESCRIPTION 
+ Installiert Powershell Module msgDSM7Module 
+
+#> 
 [CmdletBinding()]
 Param(
 )
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-not $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}"' -f ($myinvocation.MyCommand.Definition))
-exit
+	Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}"' -f ($myinvocation.MyCommand.Definition))
+	exit
 }
 $scriptpath = Split-Path -parent $MyInvocation.MyCommand.Definition
-$programpath = "msg services AG\DSMAutomation\Powershell\Modules"
+
 $psmodule = "msgDSM7Module"
 Import-Module "$scriptpath\$psmodule" -Force -ErrorAction SilentlyContinue
 $psmoduleget = get-module $psmodule
-write-host "Powershell Module ($psmodule) Version ($($psmoduleget.Version)) wird installiert."
-if ([Environment]::Is64BitProcess) {
-	$destpath = "$env:ProgramFiles\$programpath"
+$psmodulegetVersion = $psmoduleget.Version
+write-host "Powershell Module ($psmodule) Version ($psmodulegetVersion) wird installiert."
+if ($PSVersionTable.PSVersion.Major -ge 4) {
+	$programpath = "$ENV:Programfiles\WindowsPowerShell\Modules"
+	if (!(test-path "$programpath\$psmodule")) {
+		write-host "Erstelle Dir ($programpath\$psmodule)."
+		mkdir "$programpath\$psmodule" -Verbose:$VerbosePreference| Out-Null
+	}
+	if (!(test-path "$programpath\$psmodule\$psmodulegetVersion")) {
+		write-host "Erstelle Dir ($programpath\$psmodule\$psmodulegetVersion)."
+		mkdir "$programpath\$psmodule\$psmodulegetVersion" -Verbose:$VerbosePreference| Out-Null
+	}
+
+	if (!(test-path "$programpath\$psmodule\$psmodulegetVersion\de-DE")) {
+		write-host "Erstelle Dir ($programpath\$psmodule\$psmodulegetVersion\de-DE)."
+		mkdir "$programpath\$psmodule\$psmodulegetVersion\de-DE" -Verbose:$VerbosePreference| Out-Null
+	}
+	$destpath = "$programpath\$psmodule\$psmodulegetVersion"
 }
 else {
-	$destpath = "${env:ProgramFiles(x86)}\$programpath"
-}
-if (!(test-path "$destpath\$psmodule")) {
-	write-host "Erstelle Dir ($destpath)."
-	mkdir "$destpath\$psmodule" -Verbose:$VerbosePreference| Out-Null
-}
-if (!(test-path "$destpath\$psmodule\de-DE")) {
-	write-host "Erstelle Dir ($destpath)."
-	mkdir "$destpath\$psmodule\de-DE" -Verbose:$VerbosePreference| Out-Null
+	$programpath = "$env:ProgramFiles\msg services AG\DSMAutomation\Powershell\Modules"
+	if (!(test-path "$programpath\$psmodule")) {
+		write-host "Erstelle Dir ($programpath\$psmodule)."
+		mkdir "$programpath\$psmodule" -Verbose:$VerbosePreference| Out-Null
+	}
+	if (!(test-path "$programpath\$psmodule\de-DE")) {
+		write-host "Erstelle Dir ($programpath\$psmodule\de-DE)."
+		mkdir "$programpath\$psmodule\de-DE" -Verbose:$VerbosePreference| Out-Null
+	}
+	$destpath = "$programpath\$psmodule"
 }
 write-host "Kopiere Module nach ($destpath)."
-Copy-Item "$scriptpath\*.psd1" "$destpath\$psmodule" -Verbose:$VerbosePreference
-Copy-Item "$scriptpath\*.psm1" "$destpath\$psmodule" -Verbose:$VerbosePreference
-Copy-Item "$scriptpath\de-DE\*.txt" "$destpath\$psmodule\de-DE" -Verbose:$VerbosePreference 
-Copy-Item "$scriptpath\de-DE\*.xml" "$destpath\$psmodule\de-DE" -Verbose:$VerbosePreference 
+Copy-Item "$scriptpath\*.psd1" "$destpath" -Verbose:$VerbosePreference
+Copy-Item "$scriptpath\*.psm1" "$destpath" -Verbose:$VerbosePreference
+Copy-Item "$scriptpath\de-DE\*.txt" "$destpath\de-DE" -Verbose:$VerbosePreference 
+Copy-Item "$scriptpath\de-DE\*.xml" "$destpath\de-DE" -Verbose:$VerbosePreference 
 
 $p = [Environment]::GetEnvironmentVariable("PSModulePath","Machine")
-if ($p -notlike "*$destpath*"){
-	write-host "Erweitere PSModulePath ($destpath)."
-	$p += ";$destpath"
+if ($p -notlike "*$programpath*"){
+	write-host "Erweitere PSModulePath ($programpath)."
+	$p += ";$programpath"
 	[Environment]::SetEnvironmentVariable("PSModulePath",$p,"Machine")
 }
 
 # SIG # Begin signature block
 # MIIEMQYJKoZIhvcNAQcCoIIEIjCCBB4CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUluDxeiwPugwH2QwcBkU1z2rW
-# +yegggJAMIICPDCCAamgAwIBAgIQUW95fLQCIbVOuAnpDDc4ZTAJBgUrDgMCHQUA
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQURKIzPv8OSOfwu93P7kIava+e
+# wFmgggJAMIICPDCCAamgAwIBAgIQUW95fLQCIbVOuAnpDDc4ZTAJBgUrDgMCHQUA
 # MCcxJTAjBgNVBAMTHFV3ZSBGcmFua2UgKG1zZyBzZXJ2aWNlcyBBRykwHhcNMTcw
 # MjAxMTQwNjQxWhcNMzkxMjMxMjM1OTU5WjAnMSUwIwYDVQQDExxVd2UgRnJhbmtl
 # IChtc2cgc2VydmljZXMgQUcpMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1
@@ -66,9 +116,9 @@ if ($p -notlike "*$destpath*"){
 # gItg/dZ0MYIBWzCCAVcCAQEwOzAnMSUwIwYDVQQDExxVd2UgRnJhbmtlIChtc2cg
 # c2VydmljZXMgQUcpAhBRb3l8tAIhtU64CekMNzhlMAkGBSsOAwIaBQCgeDAYBgor
 # BgEEAYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEE
-# MBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBTz
-# MK0Tsk0oYNQZsoQ/57vVwX8uOTANBgkqhkiG9w0BAQEFAASBgC0MTwUdlEcZdXar
-# 52XUzF+OfnuqUPQ12qUJNrWsNwFXO6fz5c/SqoUOOrY8CR5okozNtlo412ev9NdC
-# DtxaNxZtnWzkVm4ISgB5YCIDtNWQn6lEc0cX18UeW+ukry4113pjYxl6aRaOLPSz
-# Vv3GVBPBWDvbjDNbf2Og2YYqsuPg
+# MBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBS3
+# Ue8HCmxe/OIeQtbZrrZcCC6+hDANBgkqhkiG9w0BAQEFAASBgDsk0XusYFgnx5MQ
+# k3dn0QThLGARhfxWD+I/efXdzUeAFejw1J8pq77lhQfIEDES7k1UOMoknDz9pVHb
+# bzNSZ5tc8jGOa/TcBXdvrwvMAfa0qa2ZbSY8oYMDiVoCDCuhelneK7r3qfgZnc9U
+# qxElBgylC+STmdyiuFzf1U95pOu/
 # SIG # End signature block
